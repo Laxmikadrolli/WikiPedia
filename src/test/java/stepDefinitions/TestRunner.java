9f820882e8693f63976da6bcfffae8ab0cbf4948
Laxmikadrolli <laxmi.kadrolli17@gmail.com>
2022-06-04 18:51:05 +1000

    Updated the Tag

--- src/test/java/stepDefinitions/TestRunner.java
+++ src/test/java/stepDefinitions/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/wikipedia.feature", glue = {"stepDefinitions"}, tags="@tag5",
+@CucumberOptions(features = "src/test/resources/features/wikipedia.feature", glue = {"stepDefinitions"}, tags="@tag",
 plugin = {"pretty","html:target/HTMLReports.html"})
 public class TestRunner {
 
